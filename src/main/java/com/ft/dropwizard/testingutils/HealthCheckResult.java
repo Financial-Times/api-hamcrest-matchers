df8877f789f51359b45f366bac30bbda9fad1b12
Mike Huniewicz <michal.huniewicz@ft.com>
2014-03-27 10:46:07 +0000

    Added a health check result matcher.

---
+++ src/main/java/com/ft/dropwizard/testingutils/HealthCheckResult.java
@@ -0,0 +1,75 @@
+package com.ft.dropwizard.testingutils;
+
+import com.yammer.metrics.core.HealthCheck;
+import org.hamcrest.Description;
+import org.hamcrest.Factory;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+public class HealthCheckResult extends TypeSafeMatcher<HealthCheck.Result> {
+
+	private static final boolean HEALTHY = true;
+	private static final boolean UNHEALTHY = false;
+	private static final String HEALTHY_DESCRIPTION = "healthy";
+	private static final String UNHEALTHY_DESCRIPTION = "unhealthy";
+
+	private boolean healthy;
+	private String description;
+	private String errorMessage;
+	private String errorDetail;
+
+	private HealthCheckResult(boolean healthy, String description) {
+		this(healthy, description, null);
+	}
+
+	private HealthCheckResult(boolean healthy, String description, String errorMessage) {
+		this(healthy, description, errorMessage, null);
+	}
+
+	private HealthCheckResult(boolean healthy, String description, String errorMessage, String errorDetail) {
+		this.healthy = healthy;
+		this.description = description;
+		this.errorMessage = errorMessage;
+		this.errorDetail = errorDetail;
+	}
+
+	@Override
+	protected boolean matchesSafely(HealthCheck.Result result) {
+		boolean correctResult = result != null && result.isHealthy() == healthy;
+		String message = result == null ? null : result.getMessage();
+		if (errorMessage == null) {
+			return correctResult;
+		} else if(errorDetail == null) {
+			return correctResult && message.contains(errorMessage);
+		} else {
+			return correctResult && message.contains(errorMessage) && message.contains(errorDetail);
+		}
+	}
+
+	@Override
+	public void describeTo(Description description) {
+		if (errorMessage == null) {
+			description.appendText(this.description);
+		} else {
+			description.appendText(String.format("%s with error message containing [%s]", this.description, errorMessage));
+			if(errorDetail != null) {
+				description.appendText(String.format(", and the detail: [%s]", errorDetail));
+			}
+		}
+	}
+
+	@Factory
+	public static Matcher<HealthCheck.Result> healthy() {
+		return new HealthCheckResult(HEALTHY, HEALTHY_DESCRIPTION);
+	}
+
+	@Factory
+	public static Matcher<HealthCheck.Result> unhealthy(String errorMessage) {
+		return new HealthCheckResult(UNHEALTHY, UNHEALTHY_DESCRIPTION, errorMessage);
+	}
+
+	@Factory
+	public static Matcher<HealthCheck.Result> unhealthy(String errorMessage, String errorDetail) {
+		return new HealthCheckResult(UNHEALTHY, UNHEALTHY_DESCRIPTION, errorMessage, errorDetail);
+	}
+}
---
+++ src/test/java/com/ft/dropwizard/testingutils/HealthCheckResultTest.java
@@ -0,0 +1,40 @@
+package com.ft.dropwizard.testingutils;
+
+import com.yammer.metrics.core.HealthCheck;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class HealthCheckResultTest {
+
+	@Test
+	public void healthyResultShouldMatchHealthy() {
+		assertThat(HealthCheckResult.healthy().matches(HealthCheck.Result.healthy()), is(true));
+	}
+
+	@Test
+	public void unhealthyResultShouldMatchUnhealthy() {
+		assertThat(HealthCheckResult.unhealthy("does not exist").matches(HealthCheck.Result.unhealthy("does not exist")),
+				is(true));
+	}
+
+	@Test
+	public void unhealthyResultShouldNotMatchUnhealthyWithWrongMessage() {
+		assertThat(HealthCheckResult.unhealthy("does not exist").matches(HealthCheck.Result.unhealthy("not healthy")),
+				is(false));
+	}
+
+	@Test
+	public void unhealthyResultShouldNotMatchUnhealthyWithWrongDetailedMessage() {
+		assertThat(HealthCheckResult.unhealthy("does not exist", "filenotfound").matches(HealthCheck.Result.unhealthy("does not exist")),
+				is(false));
+	}
+
+	@Test
+	public void unhealthyResultShouldMatchUnhealthyWithCorrectDetailedMessage() {
+		assertThat(HealthCheckResult.unhealthy("does not exist", "filenotfound").matches(
+				HealthCheck.Result.unhealthy("does not exist, filenotfound")), is(true));
+	}
+
+}
